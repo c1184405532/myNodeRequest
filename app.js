18f9e281099a184733af3bd19561eb814efbb504
c1184405532 <1184405532@qq.com>
2020-04-26 08:56:03 +0800

    添加put delete请求 增加token请求头字段

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ app.all('*',function(req,res,next){
     
     
     res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    
     res.header('Access-Control-Allow-Credentials','true');
     res.header('Access-Control-Allow-Methods','PUT,GET,POST,DELETE,OPTIONS');
-    
-    // res.header('Access-Control-Allow-Headers','Content-Type,username');
+    //首部字段用于预检请求的响应。指明实际请求中允许携带的首部字段。
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild,X-Access-Token');
     next();
 })
 app.get('/',function(req,res){
@@ -49,6 +49,56 @@ app.post('/api/login', function (req, res) {
     })
 })
   
+app.put('/api/add/list', function (req, res) {
+    console.log("主页 put 请求");
+    var reqData = '';
+    var resData = {};
+    req.on('data', function(chunk){    
+        reqData += chunk;
+    });
+    req.on('end',function(){
+        console.log('req',reqData)
+        reqData = JSON.parse(reqData)
+        console.log(typeof reqData)
+        if(Array.isArray(reqData.list)){
+            resData.status = 200; 
+            resData.success = true;
+            
+            resData.message = '添加成功'; 
+        }else{
+            resData.status = 400; 
+            resData.sucess = false;
+            resData.message = '数据格式错误'; 
+        }
+        res.send(resData);
+    })
+})
+
+app.delete('/api/delete/list', function (req, res) {
+    console.log("主页 put 请求");
+    var reqData = '';
+    var resData = {};
+    req.on('data', function(chunk){    
+        reqData += chunk;
+    });
+    req.on('end',function(){
+        console.log('req',reqData)
+        reqData = JSON.parse(reqData)
+        console.log(typeof reqData)
+        if(Array.isArray(reqData.ids)){
+            resData.status = 200; 
+            resData.success = true;
+            
+            resData.message = '删除成功'; 
+        }else{
+            resData.status = 400; 
+            resData.sucess = false;
+            resData.message = '数据格式错误'; 
+        }
+        res.send(resData);
+    })
+})
+
 var server = app.listen(9999,function(){
     var host = server.address().address;
     var port = server.address().port;
